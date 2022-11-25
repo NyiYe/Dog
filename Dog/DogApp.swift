4682aff0cb3e486ef6dafe2cb756314110f8aff6
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2022-11-25 12:44:36 +0630

    Initial Commit

---
+++ Dog/DogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogApp.swift
+//  Dog
+//
+//  Created by Nyi Ye Han on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
