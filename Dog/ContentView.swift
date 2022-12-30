57e31da0b88bd48dc51c041775922dd026944cec
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2022-12-30 11:34:07 +0630

    add profile one

--- Dog/ContentView.swift
+++ Dog/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             }
             
 
-            Text("Feature Chat-Profile")
+            Text("Feature Chat-Profile1")
         }
         .onAppear{
             viewModel.getDogs()
